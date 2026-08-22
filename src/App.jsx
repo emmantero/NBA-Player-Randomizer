0e889762ef1cdf803114746443a95e0f92524757
Emman Tero <emman.tero@gmail.com>
2026-08-22 19:58:17 +0800

    first commit for project: NBA Player Randomizer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <header className="page-header">
+        <h1>NBA Player Randomizer</h1>
+        <p>Build the Ultimate Player</p>
+      </header>
+    </main>
+  )
+}
+
+export default App
